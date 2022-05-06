6c63e083150568f64997ff12a2d5aeb0465a582e
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-06 14:45:45 +0530

    updated

--- assignment/src/Components/Nav.jsx
+++ assignment/src/Components/Nav.jsx
@@ -4,6 +4,7 @@ function Nav() {
     return (
       <div className="App">
         <div className="main-navbar">
+            <div className="main-navbar-1">
            <div>
                <h4>Demo Custom App</h4>
                <p>APP NAME</p>
@@ -19,12 +20,17 @@ function Nav() {
            <div>
                <h4>Button Rules</h4>
            </div>
+
+           </div>
+           <div className="main-navbar-2">
            <div>
                <p>App saved on {}</p>
            </div>
            <div>
                <button>DONE</button>
            </div>
+          
+        </div>
         </div>
       </div>
     );
