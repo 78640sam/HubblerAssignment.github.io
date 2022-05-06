62729ebc94d8430866e8ab4d61b6b9137869ec32
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-06 15:34:07 +0530

    updated

---
+++ assignment/src/Components/Actions.jsx
@@ -0,0 +1,10 @@
+
+function Actions() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
+  export default Actions;
---
+++ assignment/src/Components/Conditions.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Conditions() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
+  export default Conditions;
