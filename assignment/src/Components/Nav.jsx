7bf096119be6e38baeb1bc59a38cb58c135e1f64
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-06 14:36:38 +0530

    updated

--- assignment/src/Components/Nav.jsx
+++ assignment/src/Components/Nav.jsx
@@ -1,13 +1,35 @@
-
+import './style.css';
 
 function Nav() {
     return (
       <div className="App">
-        
+        <div className="main-navbar">
+           <div>
+               <h4>Demo Custom App</h4>
+               <p>APP NAME</p>
+           </div>
+           <div>
+           <h4>Assessment</h4>
+               <p>STAGE</p>
+           </div>
+           <div>
+           <h4>Create PO</h4>
+               <p>Button</p>
+           </div>
+           <div>
+               <h4>Button Rules</h4>
+           </div>
+           <div>
+               <p>App saved on {}</p>
+           </div>
+           <div>
+               <button>DONE</button>
+           </div>
+        </div>
       </div>
     );
   }
   
   export default Nav;
-  
+
   
---
+++ assignment/src/Components/Rules.jsx
@@ -0,0 +1,11 @@
+
+function Rules() {
+    return (
+      <div className="App">
+       
+       <input type="text" className="rules-input-box"/>
+      </div>
+    );
+  }
+  
+  export default Rules;
